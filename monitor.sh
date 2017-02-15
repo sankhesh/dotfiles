78ce308ecf8b89dc3325fe0b6a046079096bd72c
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2017-02-15 17:19:37 -0500

    Improve monitor switch script to have a cli

--- monitor.sh
+++ monitor.sh
@@ -4,33 +4,61 @@
 # Run xrandr to figure out the current monitor configuration as well as the
 # ports / identifiers for each monitor.
 
-# Turn off
-echo "Three monitors connected."
-echo "Turn on Samsung: 1"
-echo "Turn off Samsung: 2"
-echo "Turn on Dell : 3"
-echo "Turn off Dell : 4"
-echo "Enter option:"
-read option
-if [ $option = 1 ];
-  then
-    xrandr --output DVI-I-1 --off --noprimary
-    xrandr --output DP-0 --off
-    xrandr --output DP-3 --auto --primary
+function usage {
+ echo -e "Usage:"
+ echo -e "\tThree monitors connected."
+ echo -e "\tTurn on Samsung: 1"
+ echo -e "\tTurn off Samsung: 2"
+ echo -e "\tTurn on Dell : 3"
+ echo -e "\tTurn off Dell : 4"
+}
 
-elif [ $option = 2 ];
-  then
-    xrandr --output DP-3 --off --noprimary
-    xrandr --output DVI-I-1 --auto --primary
-    xrandr --output DP-0 --auto --right-of DVI-I-1
-    # Push the primary screen lower to align the bottom edges of the two
-    # screens. Note that doing this in the same command as above does not seem
-    # to work.
-    xrandr --output DVI-I-1 --pos 0x120
-elif [ $option = 3 ];
-  then
-    xrandr --output DP-0 --auto --right-of DVI-I-1
-elif [ $option = 4 ];
-  then
-    xrandr --output DP-0 --off
+function contains {
+  options="1 2 3 4"
+  [[ $options =~ (^|[[:space:]])$1($|[[:space:]]) ]] && echo 0 || echo 1
+}
+
+function switch {
+  case $1 in
+    1)
+      xrandr --output DVI-I-1 --off --noprimary
+      xrandr --output DP-0 --off
+      xrandr --output DP-3 --auto --primary
+    ;; 
+    2)
+      xrandr --output DP-3 --off --noprimary
+      xrandr --output DVI-I-1 --auto --primary
+      xrandr --output DP-0 --auto --right-of DVI-I-1
+      # Push the primary screen lower to align the bottom edges of the two
+      # screens. Note that doing this in the same command as above does not seem
+      # to work.
+      xrandr --output DVI-I-1 --pos 0x120
+    ;;
+    3)
+      xrandr --output DP-0 --auto --right-of DVI-I-1
+    ;;
+    4)
+      xrandr --output DP-0 --off
+    ;;
+  esac
+}
+
+if [ "$#" -ne 0 ]; then
+  echo "Parsing arugment: " $1
+  opt=$1
+else
+  usage
+  echo "Enter option:"
+  read option
+  opt=$option
 fi
+
+if [[ $(contains $opt) =~ 0 ]]; then
+  switch $opt
+  exit 0
+else
+  echo -e "\nERROR: Invalid option" "$opt\n"
+  usage
+  exit 1
+fi
+
