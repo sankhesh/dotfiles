775b480c5e30492841de4c774409d09035f4e955
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2017-08-01 17:37:04 -0400

    Pyclewn plugin installation

--- vim_pyclewn.sh
+++ vim_pyclewn.sh
@@ -1,21 +1,31 @@
 #!/usr/bin/env bash
 
+
 # Test if pyclewn is installed on the system
 if [[ `python -c 'import pkgutil; \
   print(1 if pkgutil.find_loader("clewn") else 0)'` > "0" ]]; then
-  echo "Pyclewn is installed"
+  printf "Pyclewn is installed.\nInstalling vim plugin..."
 else
   echo -e "Pyclewn not installed on the system.\n"\
     "\n\t $ pip install pyclewn"
   exit 1
 fi
 
-# Get the pyclewn vimball
+(
+## Get the pyclewn vimball
 export VB=`python -c "import clewn; clewn.get_vimball()" \
   | sed -E "s/.* (\/.*.vmb)$/\\1/"`
-echo $VB
 mkdir -p $HOME/.vim/bundle/pyclewn # && cd $HOME/.vim/bundle/pyclewn
 
 # Install the pyclewn plugin vimball
 (vim --cmd "set rtp^=~/.vim/bundle/pyclewn" \
-  $VB -c "UseVimball ~/.vim/bundle/pyclewn" +q)
+  $VB -c "UseVimball ~/.vim/bundle/pyclewn" +q >/dev/tty </dev/tty)
+)
+printf "Done\n"
+
+if [[ $1 != '--dont-clean' ]]; then
+  printf "Cleaning up vimball ..."
+  rm -f .Vimball.sw*
+  rm $VB
+  printf "Done\n"
+fi
