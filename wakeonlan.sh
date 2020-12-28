a7bb6dca5e180c5050018fc1809d9593dfb62bb6
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2020-12-28 09:07:15 -0500

    Simplify wakeonlan script

--- wakeonlan.sh
+++ wakeonlan.sh
@@ -6,21 +6,22 @@ sanganak=90:b1:1c:8e:15:56
 macbook=48:d7:05:ce:a7:97
 # macbook=32:00:10:65:a0:00
 
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-  wol="wol"
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-  wol="wakeonlan"
-# elif [[ "$OSTYPE" == "cygwin" ]]; then
-#         # POSIX compatibility layer and Linux environment emulation for Windows
-# elif [[ "$OSTYPE" == "msys" ]]; then
-#         # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
-# elif [[ "$OSTYPE" == "win32" ]]; then
-#         # I'm not sure this can happen.
-# elif [[ "$OSTYPE" == "freebsd"* ]]; then
-#         # ...
-# else
-        # Unknown.
-fi
+wol=wakeonlan
+# if [[ "$OSTYPE" == "linux-gnu" ]]; then
+#   wol="wakeonlan"
+# elif [[ "$OSTYPE" == "darwin"* ]]; then
+#   wol="wakeonlan"
+# # elif [[ "$OSTYPE" == "cygwin" ]]; then
+# #         # POSIX compatibility layer and Linux environment emulation for Windows
+# # elif [[ "$OSTYPE" == "msys" ]]; then
+# #         # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+# # elif [[ "$OSTYPE" == "win32" ]]; then
+# #         # I'm not sure this can happen.
+# # elif [[ "$OSTYPE" == "freebsd"* ]]; then
+# #         # ...
+# # else
+#        # Unknown.
+# fi
 
 echo "Which PC to wake?"
 echo "s) sthir"
