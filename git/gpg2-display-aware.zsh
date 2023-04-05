22c45ec82031cc321138fda10c98229d6a615ada
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2023-04-04 23:09:50 -0400

    GIT: Use display aware pinentry for gpg signing

---
+++ git/gpg2-display-aware.zsh
@@ -0,0 +1,20 @@
+#!/usr/bin/env zsh
+
+# Script to automatically configure whether to invoke a graphical UI or a command-line interface for
+# password entry when signing git commits using GPG.
+#
+# To use ensure that git config uses this script like so:
+#
+#     git config --global gpg.program "~/.gpg2-display-aware.zsh
+#
+# Source: https://ertt.ca/blog/2022/01-10-git-gpg-ssh/
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  if [ -n "$DISPLAY" ]; then
+    exec gpg "$@"
+  else
+    exec gpg --pinentry-mode loopback "$@"
+  fi
+elif [[ "$OSTYPE" == "win32" ]]; then
+  exec gpg.exe $@
+fi
