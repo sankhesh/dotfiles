f834c125d840c67865e986edf4235126a9769317
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2024-02-13 12:37:28 -0500

    Install: Break system packages for pip install of lint libraries

--- pypackages/pythonpackages.zsh
+++ pypackages/pythonpackages.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-set PYTHON_EXE=python
+PYTHON_EXE=python
 if grep -qi microsoft /proc/version || [[ "$OSTYPE" == "win32" ]]; then
   # For WSL, use python.exe
   PYTHON_EXE=python.exe
@@ -10,4 +10,9 @@ fi
 # Using a relative path ensures that this script works across path difference in
 # linux (eg: /home/..), wsl (eg: /mnt/c/..) etc.
 REQPATH=`realpath --relative-to=${PWD} ${0:a:h}/requirements.txt`
-$PYTHON_EXE -m pip install -r $REQPATH
+ERROROUT="$( $PYTHON_EXE -m pip install -r $REQPATH 2>&1 > /dev/null )"
+if [[ "$ERROROUT" =~ externally-managed-environment ]];
+then
+  echo "Breaking system packages."
+  $PYTHON_EXE -m pip install --break-system-packages -r $REQPATH
+fi
