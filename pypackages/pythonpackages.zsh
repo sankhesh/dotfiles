126cbe48dc821a298dc13dbc42513a409e5c1b01
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2025-04-10 12:19:34 -0400

    Use python's pathlib for posix path

--- pypackages/pythonpackages.zsh
+++ pypackages/pythonpackages.zsh
@@ -10,11 +10,16 @@ fi
 # Get the relative path to the requirements file
 # Using a relative path ensures that this script works across path difference in
 # linux (eg: /home/..), wsl (eg: /mnt/c/..) etc.
-relpath(){ $PYTHON_EXE -c "import os.path; print(os.path.relpath('$1','${2:-$PWD}'));" }
-REQPATH=$(relpath ${0:a:h}/requirements.txt $PWD)
-ERROROUT="$( $PYTHON_EXE -m pip install -r $REQPATH )" #  2>&1 > /dev/null )"
+relpath()
+  {
+    $PYTHON_EXE -c "from pathlib import Path,PurePosixPath; print(PurePosixPath(Path('$1').relative_to(Path('${2:-${PWD:A}}'), walk_up=True)), end='');"
+  }
+REQPATH=$(relpath ${0:A:h}/requirements.txt ${PWD:A})
+ERROROUT="$( $PYTHON_EXE -m pip install -r ${REQPATH%\\r} )" #  2>&1 > /dev/null )"
 if [[ "$ERROROUT" =~ externally-managed-environment ]];
 then
   echo "Breaking system packages."
   $PYTHON_EXE -m pip install --break-system-packages -r $REQPATH
+else
+  echo $ERROROUT
 fi
