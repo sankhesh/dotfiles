145151726013f7f9319384635b906fd9c4283291
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2025-06-15 02:20:59 -0400

    NVIM: clangd compile database global and local

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -25,71 +25,41 @@ return {
       local cmp_lsp = require('cmp_nvim_lsp')
       local capabilities = cmp_lsp.default_capabilities()
 
-      -- Globally override the hover handler to make the window non-focusable
-      vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
-        border = 'rounded',
-        focusable = false,
-      })
-
       local on_attach = function(client, bufnr)
         local keymap = vim.keymap.set
         local opts = { buffer = bufnr, remap = false }
 
-        keymap('n', 'gd', function()
-          vim.lsp.buf.definition()
-        end, opts)
-        keymap('n', 'gD', function()
-          vim.lsp.buf.declaration()
-        end, opts)
-        keymap('n', 'gi', function()
-          vim.lsp.buf.implementation()
-        end, opts)
-        keymap('n', 'K', function()
-          vim.lsp.buf.hover()
-        end, opts)
-        keymap('n', '<leader>ws', function()
-          vim.lsp.buf.workspace_symbol()
-        end, opts)
-        keymap('n', '<leader>vd', function()
-          vim.diagnostic.open_float()
-        end, opts)
-        keymap('n', '[d', function()
-          vim.diagnostic.goto_next()
-        end, opts)
-        keymap('n', ']d', function()
-          vim.diagnostic.goto_prev()
-        end, opts)
-        keymap('n', '<leader>ca', function()
-          vim.lsp.buf.code_action()
-        end, opts)
-        keymap('n', '<leader>rf', function()
-          vim.lsp.buf.references()
-        end, opts)
-        keymap('n', '<leader>rn', function()
-          vim.lsp.buf.rename()
-        end, opts)
-        keymap('i', '<C-h>', function()
-          vim.lsp.buf.signature_help()
-        end, opts)
+        -- Your custom keymaps
+        keymap('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+        keymap('n', 'gD', function() vim.lsp.buf.declaration() end, opts)
+        keymap('n', 'gi', function() vim.lsp.buf.implementation() end, opts)
+        keymap('n', 'K', function() vim.lsp.buf.hover() end, opts)
+        keymap('n', '<leader>ws', function() vim.lsp.buf.workspace_symbol() end, opts)
+        keymap('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
+        keymap('n', '[d', function() vim.diagnostic.goto_next() end, opts)
+        keymap('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
+        keymap('n', '<leader>ca', function() vim.lsp.buf.code_action() end, opts)
+        keymap('n', '<leader>rf', function() vim.lsp.buf.references() end, opts)
+        keymap('n', '<leader>rn', function() vim.lsp.buf.rename() end, opts)
+        keymap('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
 
-        -- Adding the inlay hints keymap
+        -- Inlay hints keymap
         keymap('n', '<leader>ih', function()
           vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
         end, { desc = 'Toggle Inlay Hints' })
 
-        -- Enable inlay hints on attach if the server supports it
         if client.supports_method('textDocument/inlayHint') then
           vim.lsp.inlay_hint.enable(true)
         end
 
-        -- Add autocommand for automatic hover documentation
-        vim.api.nvim_create_autocmd('CursorHold', {
+        -- Automatic hover
+        vim.api.nvim_create_autocmd("CursorHold", {
           buffer = bufnr,
           callback = function()
             if vim.api.nvim_get_mode().mode == 'n' then
               local diagnostics = vim.diagnostic.get(bufnr, { lnum = vim.fn.line('.') - 1 })
               if #diagnostics > 0 then
-                vim.diagnostic.open_float(nil, { scope = 'line', focusable = false })
+                vim.diagnostic.open_float(nil, { scope = "line", focusable = false })
               else
                 vim.lsp.buf.hover()
               end
@@ -97,6 +67,15 @@ return {
           end,
         })
       end
+
+      -- Globally override hover handler to be non-focusable
+      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+        vim.lsp.handlers.hover,
+        {
+          border = "rounded",
+          focusable = false,
+        }
+      )
 
       -- Setup servers based on what is installed by mason-lspconfig
       local servers = require('mason-lspconfig').get_installed_servers()
@@ -106,12 +85,24 @@ return {
           capabilities = capabilities,
         }
 
-        -- Add specific configuration for clangd
+        -- Add custom logic for clangd to handle project-specific configs
         if server_name == 'clangd' then
-          server_opts.cmd = {
-            'clangd',
-            '--compile-commands-dir=../bld', -- Point to the build directory
-          }
+          -- Try to find the project root (e.g., the git repository root)
+          local root_dir = require('lspconfig.util').find_git_ancestor(vim.api.nvim_buf_get_name(0))
+          if root_dir then
+            -- Check if a .clangd file exists in the project root
+            local clangd_config_exists = vim.loop.fs_stat(root_dir .. '/.clangd')
+
+            -- If a .clangd file does NOT exist, apply the global default settings.
+            if not clangd_config_exists then
+              server_opts.cmd = {
+                'clangd',
+                '--compile-commands-dir=../bld', -- Your global default build directory
+              }
+            end
+            -- If a .clangd file DOES exist, we don't add any 'cmd' options,
+            -- allowing clangd to find and use it automatically.
+          end
         end
 
         lspconfig[server_name].setup(server_opts)
