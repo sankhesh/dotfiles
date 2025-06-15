c6e039310e5b27b0cfcab90e955dfd60fb438076
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2025-06-15 00:41:44 -0400

    NVIM: Use vscode colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -12,6 +12,20 @@ return {
         --   comments = "italic",
         --   keywords = "bold",
         -- }
+        -- Enable transparent background
+        transparent = true,
+
+        -- Enable italic comment
+        italic_comments = true,
+
+        -- Underline `@markup.link.*` variants
+        underline_links = true,
+
+        -- Disable nvim-tree background color
+        disable_nvimtree_bg = true,
+
+        -- Apply theme colors to terminal
+        terminal_colors = true,
       })
       vim.cmd("colorscheme vscode")
     end
