fa12c3d78b1f782469e3b73067aae268517cc8ae
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2025-07-08 02:35:49 -0400

    NVIM: Better treesitter based folding

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,28 +1,29 @@
 -- init.lua
 
 -- Set leader key
-vim.g.mapleader = ","
+vim.g.mapleader = ','
 
 -- Load core configuration
-require("core.options")
-require("core.keymaps")
+require('core.options')
+require('core.keymaps')
+require('core.folding')
 
 -- Bootstrap lazy.nvim and load plugins
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable',
     lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Load plugin specifications and enable LuaRocks support
-require("lazy").setup("plugins", {
+require('lazy').setup('plugins', {
   rocks = {
     -- Enable lazy.nvim to manage LuaRocks dependencies for plugins
     enabled = true,
--- nvim/lua/core/folding.lua
+++ nvim/lua/core/folding.lua
@@ -1,44 +1,54 @@
-" =============================================================================
-" Folding, :help fold -> All fold commands start with z
-" Options for folds start at :help fcl
-set foldenable
+-- lua/core/folding.lua
 
-" auto open folds in these conditions
-set foldopen=block,insert,jump,mark,percent,quickfix,search,tag,undo
+local M = {} -- Create a module table
 
-function! MyFoldText()
-    let line = getline(v:foldstart)
-    let numberOfLines = 1 + v:foldend - v:foldstart
-    let sub = substitute(line, '/\*\|\*/\|{{{\d\=', '', 'g')
-    return v:folddashes .. sub .. ' (' .. numberOfLines .. ' Lines)'
-endfunction
+-- General folding options
+vim.opt.foldenable = true
+vim.opt.foldopen = 'block,insert,jump,mark,percent,quickfix,search,tag,undo'
 
-if has('nvim')
-    " Treesitter folding
-    " https://www.reddit.com/r/neovim/comments/16xz3q9/treesitter_highlighted_folds_are_now_in_neovim/
-    set foldmethod=expr
-    set foldexpr=v:lua.vim.treesitter.foldexpr()
-    " set foldtext=v:lua.vim.treesitter.foldtext()
-    set foldtext=MyFoldText()
-else
-    set foldmethod=syntax       " Fold based on indention levels.
-    set foldtext=MyFoldText()
-endif
+-- Folds with a level > foldlevel will be closed
+-- Setting 0 will close all folds
+-- Setting 99 ensures folds are open by default
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = -1
+vim.opt.foldnestmax = 5 -- Only fold up to this many nested levels.
+vim.opt.foldminlines = 1 -- Only fold if there are at least this many lines.
+vim.opt.fillchars = {
+  fold = '·',
+  foldopen = '',
+  foldclose = '',
+}
 
+-- Custom foldtext function in Lua
+-- This function is called by 'foldtext' option to display folded lines.
+-- It's now part of the 'M' module table to be accessible via require().
+M.MyFoldText = function()
+  local line = vim.fn.getline(vim.v.foldstart)
+  local numberOfLines = 1 + vim.v.foldend - vim.v.foldstart
+  -- Remove fold markers (/\* \*/ {{{d=) from the line
+  local sub = string.gsub(line, '/%*|%*/|%{%{%{%d*=', '')
+  -- Return the formatted fold text
+  return vim.v.folddashes .. sub .. ' (' .. numberOfLines .. ' Lines)'
+end
 
-" Folds with a level > foldlevel will be closed
-" Setting 0 will close all folds
-" Setting 99 ensures folds are open by default
-set foldlevel=99
-set foldlevelstart=-1
-set foldnestmax=5               " Only fold up to this many nested levels.
-set foldminlines=1              " Only fold if there are at least this many lines.
-set fillchars=fold:·,foldopen:,foldclose:
+-- Treesitter folding (recommended for Neovim)
+-- This sets the fold method to 'expr' and uses the Treesitter fold expression.
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+-- Reference the Lua function using v:lua.require('module_name').function_name()
+vim.opt.foldtext = 'v:lua.require("core.folding").MyFoldText()'
 
-" alias for fold current cursor position for convenience
-nnoremap <silent> zz  za
+-- Keymap for toggling folds (alias for 'za')
+vim.keymap.set('n', 'zz', 'za', { silent = true, desc = 'Toggle fold under cursor' })
 
-highlight Folded ctermfg=14 ctermbg=236 gui=underdouble guisp=#008080 guifg=#00FFFF guibg=#303030
+-- Highlight group definitions for folds and sign column
+-- These can also be managed by your colorscheme, but are included for direct translation.
+vim.api.nvim_set_hl(
+  0,
+  'Folded',
+  { fg = '#00FFFF', bg = '#303030', underline = true, undercurl = true, sp = '#008080' }
+)
+vim.api.nvim_set_hl(0, 'SignColumn', { fg = '#00FFFF', bg = '#1C1C1C' })
+vim.api.nvim_set_hl(0, 'LineNr', { fg = '#FFFF00', bg = '#1C1C1C' })
 
-highlight SignColumn ctermfg=51 ctermbg=234 guifg=#00FFFF guibg=#1C1C1C
-highlight LineNr ctermfg=11 ctermbg=234 guifg=#FFFF00 guibg=#1C1C1C
+return M -- Return the module table
