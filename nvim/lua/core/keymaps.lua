0c80d9c003ee7ef0cba8e8d726c119d454ae8b72
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2025-08-25 01:43:52 -0400

    NVIM: Add keymap for Telescope resume

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -50,6 +50,7 @@ keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { desc = 'Find files'
 keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', { desc = 'Live grep' })
 keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { desc = 'Find buffers' })
 keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', { desc = 'Help tags' })
+keymap('n', '<leader>fr', '<cmd>Telescope resume<cr>', { desc = 'Resume previous find' })
 
 -- Git
 keymap('n', '<leader>gg', ':Neogit<CR>', { desc = 'Open Neogit' })
