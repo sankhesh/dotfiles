b828dff12b85a9ec488a406f5af19ce5ddc6c78d
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2025-09-17 11:12:09 -0400

    NVIM: Move linting config to separate file

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -28,27 +28,4 @@ return {
       require('Comment').setup()
     end,
   },
-
-  -- Linting
-  {
-    'mfussenegger/nvim-lint',
-    config = function()
-      local lint = require('lint')
-      lint.linters_by_ft = {
-        python = { 'flake8' },
-        cmake = { 'cmakelint' },
-        javascript = { 'eslint' },
-      }
-
-      lint.linters.cmakelint.args = {
-        '--filter=-whitespace/extra,-whitespace/indent',
-      }
-
-      vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufEnter', 'InsertLeave' }, {
-        callback = function()
-          lint.try_lint()
-        end,
-      })
-    end,
-  },
 }
