ec5acd4075557b4e07ce623cbed1e3d1e3fd710e
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2025-09-17 11:10:59 -0400

    NVIM: Toggle autoformatting via keystrokes

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,49 +1,69 @@
 -- lua/plugins/conform.lua
 
 return {
-    'stevearc/conform.nvim',
-    event = { "LspAttach", "BufReadPost", "BufNewFile" },
-    opts = {
-      log_level = vim.log.levels.DEBUG,
-      -- Add custom options for specific formatters
-      formatters = {
-        cmake_format = {
-          prepend_args = {
-            "--dangle-align=prefix",
-            "--dangle-parens=true",
-            "--enable-sort=true",
-            "--max-subgroups-hwrap=1",
-            "--max-pargs-hwrap=1",
-            "--line-width=80",
-          },
+  'stevearc/conform.nvim',
+  event = { 'LspAttach', 'BufReadPost', 'BufNewFile' },
+  opts = {
+    log_level = vim.log.levels.DEBUG,
+    -- Add custom options for specific formatters
+    formatters = {
+      cmake_format = {
+        prepend_args = {
+          '--dangle-align=prefix',
+          '--dangle-parens=true',
+          '--enable-sort=true',
+          '--max-subgroups-hwrap=1',
+          '--max-pargs-hwrap=1',
+          '--line-width=80',
         },
       },
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        c = { 'clang_format' },
-        cpp = { 'clang_format' },
-        python = { 'isort', 'black' },
-        javascript = { 'prettierd', 'prettier', 'eslint' },
-        typescript = { 'eslint' },
-        typescriptreact = { 'eslint' },
-        cmake = { 'cmake_format' },
-        ['*'] = { 'trim_whitespace' }, -- Apply to all file types
-      },
-      format_after_save = {
-        timeout_ms = 2500, -- Increased timeout from 500 to 2500ms (2.5 seconds)
-        lsp_fallback = true,
-        async = true,
-      },
     },
-  config = function (_, opts)
-    local conform = require ("conform")
+    formatters_by_ft = {
+      lua = { 'stylua' },
+      c = { 'clang_format' },
+      cpp = { 'clang_format' },
+      python = { 'isort', 'black', 'autopep8', 'autoflake' },
+      javascript = { 'prettierd', 'prettier', 'eslint' },
+      typescript = { 'eslint' },
+      typescriptreact = { 'eslint' },
+      cmake = { 'cmake_format' },
+      ['*'] = { 'trim_whitespace' }, -- Apply to all file types
+    },
+    format_on_save = function(bufnr)
+      if vim.g.disable_autoformat then
+        return
+      end
+      return {
+        timeout_ms = 2500,
+        lsp_fallback = true,
+        async = false,
+      }
+    end,
+  },
+  config = function(_, opts)
+    local conform = require('conform')
 
     -- Setup conform.nvim to work
     conform.setup(opts)
 
     -- Customize the default prettier command
     conform.formatters.prettier = {
-      prepend_args = { "--prose-wrap", "always" },
+      prepend_args = { '--prose-wrap', 'always' },
     }
+
+    vim.keymap.set({ 'n', 'v' }, '<leader>cf', function()
+      conform.format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 2500,
+      })
+    end, { desc = 'Format file or range (in visual mode)' })
+
+    vim.keymap.set(
+      { 'n' },
+      '<leader>af',
+      ':lua vim.g.disable_autoformat = not vim.g.disable_autoformat<CR>',
+      { desc = 'Toggle autoformat' }
+    )
   end,
 }
