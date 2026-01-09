cc034f9ea2d3a92931f76c373396e71ee7c95e79
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2026-01-09 10:23:24 -0500

    NVIM: Add markdown toc

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,11 +1,54 @@
 -- lua/plugins/markdown.lua
 
 return {
-  'MeanderingProgrammer/render-markdown.nvim',
-  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
-  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' }, -- if you use standalone mini plugins
-  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-  ---@module 'render-markdown'
-  ---@type render.md.UserConfig
-  opts = {},
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons',
+    },
+    opts = {},
+  },
+  {
+    'hedyhli/markdown-toc.nvim',
+    ft = { 'markdown' },
+    cmd = { 'Mtoc' },
+    -- Optional: add config or keymaps if needed
+    opts = {
+      headings = {
+        -- Include headings before the ToC (or current line for `:Mtoc insert`).
+        -- Setting to true will include headings that are defined before the ToC
+        -- position to be included in the ToC.
+        before_toc = false,
+      },
+
+      -- Table or boolean. Set to true to use these defaults, set to false to disable completely.
+      -- Fences are needed for the update/remove commands, otherwise you can
+      -- manually select ToC and run update.
+      fences = {
+        enabled = true,
+        -- These fence texts are wrapped within "<!-- % -->", where the '%' is
+        -- substituted with the text.
+        start_text = 'mtoc-start',
+        end_text = 'mtoc-end',
+        -- An empty line is inserted on top and below the ToC list before the being
+        -- wrapped with the fence texts, same as vim-markdown-toc.
+      },
+
+      -- Enable auto-update of the ToC (if fences found) on buffer save
+      auto_update = true,
+
+      toc_list = {
+        -- string or list of strings (for cycling)
+        -- If cycle_markers = false and markers is a list, only the first is used.
+        -- You can set to '1.' to use a automatically numbered list for ToC (if
+        -- your markdown render supports it).
+        markers = '*',
+        cycle_markers = false,
+        -- Example config for cycling markers:
+        ----- markers = {'*', '+', '-'},
+        ----- cycle_markers = true,
+      },
+    },
+  },
 }
