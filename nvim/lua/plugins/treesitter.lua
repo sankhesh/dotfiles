0987ffd2e7ec400635fac564aa8a803254967738
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2026-04-03 12:59:24 -0400

    NVIM: Use new treesitter changes

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,9 +3,11 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
     build = ':TSUpdate',
+    main = 'nvim-treesitter.config',
     config = function()
-      require('nvim-treesitter.configs').setup({
+      require('nvim-treesitter.config').setup({
         ensure_installed = {
           'c',
           'cpp',
